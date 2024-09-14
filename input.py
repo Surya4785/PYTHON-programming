0a9765a36a0f5db719c29e38b70a45eda5a66f71
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-15 02:10:52 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,16 @@
+name = input("enter your name:")
+print("welcome",name)
+
+age = input("enter your age:")
+print("you entered",age)
+
+val = input("enter some value:")
+print(type(val),val)
+
+int("5")
+val = int(input("enter some value:"))
+print(type(val),val)
+
+float("5.0")
+val = float(input("enter some value:"))
+print(type(val),val)
