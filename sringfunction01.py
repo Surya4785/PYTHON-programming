3b49dc47358127fa8ad32743ff0f5b42aa79165d
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-17 00:02:36 +0530

    Add files via upload

---
+++ sringfunction01.py
@@ -0,0 +1,20 @@
+str = "I am studying python from Apna College"
+
+#endswith
+print(str.endswith("ege"))
+print(str.endswith("app"))
+
+#capitalize
+print(str.capitalize())
+
+#replace
+print(str.replace("python","javascript"))
+
+#find
+print(str.find("o"))
+
+#count
+print(str.count("from"))
+
+
+
