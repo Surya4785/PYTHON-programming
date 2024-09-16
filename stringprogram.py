c7a77a4bb23cf382587c5e9fc08381cc61d667f3
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-16 23:12:50 +0530

    Add files via upload

---
+++ stringprogram.py
@@ -0,0 +1,13 @@
+str1 = "physics"
+len1 = len(str1)
+print(str1)
+print(len(str1))
+
+str2 = "wallah"
+len2 = len(str2)
+print(str2)
+print(len(str2))
+
+final_str = (str1 + " " + str2)
+print(final_str)
+print(len(final_str))
