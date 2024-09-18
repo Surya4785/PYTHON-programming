ea96cad570ad91d22f37ff70ef9e10285867002e
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-18 23:18:19 +0530

    Add files via upload

---
+++ ifelifelse.py
@@ -0,0 +1,10 @@
+light = input("enter the signal colour")
+
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+   print("go")
+elif(light == "yrllow"):
+   print("look")
+else:
+   print("light is broken")
