9193b69152bc6efc0cfdbf5389a56679b41713db
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-18 23:47:45 +0530

    Add files via upload

---
+++ ifelifelsequiz01.py
@@ -0,0 +1,12 @@
+marks = int(input("enter student marks :"))
+
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 80 and marks < 90):
+    grade = "B"
+elif(marks >= 70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+
+    print("the grade of the student is:",grade)
