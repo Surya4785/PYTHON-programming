34491d716ce7272b8b905caf980e8db70df3ecc8
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-21 00:35:54 +0530

    Add files via upload

---
+++ listingmethods.py
@@ -0,0 +1,41 @@
+#list.append
+
+list = [2, 1, 3]
+list.append(4)
+print(list)
+
+#list.sort
+
+list = [2, 1, 3]
+list.append(4)
+list.sort()
+print(list)
+
+#list.sort(decending order)
+
+list = [2, 1, 3]
+list.append(4)
+list.sort(reverse=True)
+print(list)
+
+#list.reverse
+
+list = [2, 1, 3, 9, 7]
+list.reverse()
+print(list)
+
+#list.insert
+
+list = [2, 4, 7, 9]
+list.insert(1,5)
+print(list)
+
+#list.remove and list.pop
+
+list = [2, 1, 3, 1]
+list.remove(1)
+print(list)
+
+list = [2, 1, 3, 1]
+list.pop(2)
+print(list)
