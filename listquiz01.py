a78e4f449e68a17154bd6a2ac1fa553ab2c56ab0
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-23 00:08:30 +0530

    Add files via upload

---
+++ listquiz01.py
@@ -0,0 +1,12 @@
+#write a program to ask the user to enter the nsames of their 3 favourite movies and store them in a list.
+
+movies = []
+mov1 = input("enter the 1st movie:")
+mov2 = input("enter the 2nd movie:")
+mov3 = input("enter the 3rd movie")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
