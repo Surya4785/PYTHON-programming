dbdd147dd2d84eaf2ee0a29a8ba6db1648b3ee5c
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-25 00:08:11 +0530

    Add files via upload

---
+++ quiz03.py
@@ -0,0 +1,12 @@
+list = [ "New York", "Los Angles", "Boston", "Denver" ]
+
+print(list)     # prints all elements
+print(list[0])  # print first element
+
+list2 = [1,3,4,6,4,7,8,2,3]
+
+print(sum(list2))
+print(min(list2))
+print(max(list2))
+print(list2[0])
+print(list2[-1])
