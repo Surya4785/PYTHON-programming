3126a8d137fcab7ac1d866122fd8af9a7cff36ae
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-09-26 09:50:10 +0530

    Add files via upload

---
+++ quiz04.py
@@ -0,0 +1,14 @@
+#write a program to input any number and check whether it is positive, negative or zero.
+
+number = int(input("enter the number:"))
+
+if number > 0:
+    print("Positive number")
+
+elif number < 0:
+    print("Negative number")
+
+else:
+    print("Zero")
+
+    
