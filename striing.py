a818b0e3124e5182b816ac5fcc88ca73d2038238
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-10-09 10:19:13 +0530

    Add files via upload

---
+++ striing.py
@@ -0,0 +1,17 @@
+st = 'I like python'
+print(st[2:6])
+
+st = 'I like python'
+print(st[0:6:2])
+
+st = 'I like python'
+print(st[12:6:-1])
+
+st = ' I like python'
+print(st[-11:7])
+
+st = 'I like python'
+print(st[:])
+
+st = 'I like python'
+print(st[:6])
