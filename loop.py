1fcff8ec9b0721ca27d57cac8fbb75c2c2329580
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2024-10-17 23:55:32 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,22 @@
+# list using while 
+l= [1,"mohit","surya","anikesh"]
+i=0
+while(i<len(l)):
+    print(l[i])
+    i = i+1
+# for loop
+k=[1,"mohit","surya","anikesh",43]
+
+for i in k:
+    print (i)
+    
+# same  as tuple and sring
+k=[1,"mohit","surya","anikesh",43]
+
+for k in range (645):
+
+    pass
+i =0
+while(i<12):
+    print(i)
+    i=i+2
